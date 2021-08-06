7adc86499193b682c020bea98688b84a2d441da8
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-06 12:17:46 +0300

    starting work on lab 05

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -108,49 +108,42 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">+10</span>
+                          <span className="description">Projects</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className="heading">600</span>
+                          <span className="description">Course Hours</span>
                         </div>
-                        <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
-                        </div>
+                      
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Thaer AlJomhawi{" "}
+                      <span className="font-weight-light">, 26</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Amman, Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                     Full Stack Web Developer
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      LTUC-Abdul Aziz Al Ghurair School of Advanced Computing 
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                          A  Software Developer, builds websites and web Applications with JavaScript ,HTML ,CSS
                         </p>
-                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <a href="#Thaer" onClick={e => e.preventDefault()}>
                           Show more
                         </a>
                       </Col>
